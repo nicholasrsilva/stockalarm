1165e657f5d4f5ed7c2d396cb39d24d0fad89a5d
nick silva <nrs221@lehigh.edu>
2021-09-30 15:02:01 -0400

    retracted pull py

--- pull.py
+++ pull.py
@@ -2,29 +2,31 @@ import pandas
 from alpha_vantage.timeseries import TimeSeries
 import sys
 import random
+
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
-import pandas as pd
+# import pandas as pd
 import ast
 app = Flask(__name__)
 api = Api(app)
 class Users(Resource):
-    # methods go here
+    def get(self):
+        ticker = "aapl"
+        lines = open('keys').read().splitlines()
+        keys = random.choice(lines)
+        time = TimeSeries(key = keys, output_format ='pandas' )
+        data = time.get_intraday(symbol=ticker, interval ='1min', outputsize = 'full')
+
+        # data = pd.read_csv('users.csv')  # read CSV
+        data = data.to_dict()  # convert dataframe to dictionary
+        return {'data': data}, 200  # return data and 200 OK code
     pass
 
 api.add_resource(Users, '/users')  # '/users' is our entry point
 
 
-
-ticker = "aapl"
-
-lines = open('keys').read().splitlines()
-keys = random.choice(lines)
-time = TimeSeries(key = keys, output_format ='pandas' )
-data = time.get_intraday(symbol=ticker, interval ='1min', outputsize = 'full')
-
-print(ticker)
-print(data)
+# print(ticker)
+# print(data)
 
 
 if __name__ == '__main__':
