ff1ab215ab021fdafb27c5866292ba5d7ac1c4eb
nick silva <nrs221@lehigh.edu>
2021-09-30 14:35:15 -0400

    Created api resource with flask

--- pull.py
+++ pull.py
@@ -1,10 +1,22 @@
-#!/usr/bin/python3
 import pandas
 from alpha_vantage.timeseries import TimeSeries
 import sys
 import random
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+app = Flask(__name__)
+api = Api(app)
+class Users(Resource):
+    # methods go here
+    pass
 
-ticker = str(sys.argv[1])
+api.add_resource(Users, '/users')  # '/users' is our entry point
+
+
+
+ticker = "aapl"
 
 lines = open('keys').read().splitlines()
 keys = random.choice(lines)
@@ -13,3 +25,7 @@ data = time.get_intraday(symbol=ticker, interval ='1min', outputsize = 'full')
 
 print(ticker)
 print(data)
+
+
+if __name__ == '__main__':
+    app.run()  # run our Flask app
